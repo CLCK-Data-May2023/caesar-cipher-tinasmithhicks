f1df63a9d0be4bd37b93194d51f1f477e7f2038e
tinasmithhicks <127160751+tinasmithhicks@users.noreply.github.com>
2023-08-06 22:43:33 -0400

    project file

--- cipher.py
+++ cipher.py
@@ -1,1 +1,29 @@
-# add your code here
+def caesar_cipher(text, shift):
+    encrypted_text = ""
+    
+    for char in text:
+        if char.isalpha():  # check if the character is an alphabet
+            # find the position of the character in the alphabet (0-25)
+            position = ord(char) - ord('a' if char.islower() else 'A')
+            # shift the position
+            new_position = (position + shift) % 26
+            # add the shifted character to the encrypted text
+            encrypted_text += chr(new_position + ord('a' if char.islower() else 'A'))
+        else:
+            encrypted_text += char
+    
+    return encrypted_text
+
+# Test the function
+plaintext = "programming python is fun!"
+shift = 5
+encrypted_text = caesar_cipher(plaintext, shift)
+print(f"Plaintext: {plaintext}")
+print(f"Encrypted text: {encrypted_text}")
+
+
+
+
+
+
+
